710cd593d849b67a4c251128fb76cd061b4c2e5f
Rachel K. Matherly <rachel.matherly@centre.edu>
2019-11-11 20:51:25 -0500

    trying buttons

--- code.js
+++ code.js
@@ -3,16 +3,11 @@ var success=function(data)
     {
         console.log(data);
 
-        var points =data.map(function(pen, i)
-                   {
-                        return {
-                            x:i,
-                            y:pen.quizes[0].grade
-                        }
-                    })
+        var points= getGradesByDay(0,data)
+        
         console.log(points)
         
-        var screen = {width:1000, height:1000}
+        var screen = {width:500, height:500}    
         
         var setup= function(points)
             {
@@ -29,25 +24,54 @@ var success=function(data)
                 yScale.domain([d3.min(points,function(p){return p.y}) , d3.max(points,function(p){return p.y})])
                 yScale.range ([screen.height, 0])
                 
-                drawPoints(points)
+                drawPoints(points, xScale, yScale)
                 
             }
         
-        var drawPoints = function(points)
+        var drawPoints = function(points, xScale, yScale)
             {
                 d3.select("svg")
                 .selectAll("circle")
                 .data(points)
                 .enter()
                 .append("circle")
-                .attr("cx", function (p){return p.x})
-                .attr("cy", function (p){return p.y})
+                .attr("cx", function (p){return xScale(p.x)})
+                .attr("cy", function (p){return yScale(p.y)})
                 .attr("r", 4)
             }
+        
         
         setup(points)
         
         drawPoints(points)
+        
+        
+
+        
+        d3.select("#button1").on("click", function()
+                            {
+                                console.log("click")
+                                var points=getGradesByDay(0,data)
+                                console.log(points)
+                                setup(points)
+                                console.log("setup checkmark")
+                                drawPoints(points)
+                                console.log("drawing check mark")
+                            
+                            })
+        
+        d3.select("#button2").on("click", function()
+                            {
+                                console.log("click")
+                                var hey=getGradesByDay(1,data)
+                                console.log(hey)
+                                setup(hey)
+                                console.log("setup checkmark")
+                                drawPoints(hey)
+                                console.log("drawing check mark")
+                            })
+
+        
         
         
         
@@ -60,4 +84,19 @@ var fail=function(data)
 
     }
 penPromise.then(success, fail);
+
+var getGradesByDay= function(day,data)
+    {
+        var points =data.map(function(pen, i)
+                   {
+                        return {
+                            x:i,
+                            y:pen.quizes[day].grade
+                        }
+                    })
+        return points
+        
+    }
+
+
 
